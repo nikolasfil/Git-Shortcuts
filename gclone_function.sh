038af451a2e033f4e76d52d9e080dccf71ad199e
nikolasfil <nikolas13in@gmail.com>
2023-08-09 22:38:40 +0300

    updated

--- gclone_function.sh
+++ gclone_function.sh
@@ -1,1 +1,62 @@
-/home/figaro/Programms/Bash/GitShortcuts/gclone_function.sh
+#!/bin/bash
+
+
+gclone(){
+
+if [[ $1 == 'git@github.com:'* ]]
+then 
+    git clone $1
+    local var=$(trimmerPrefix "$1" "git@github.com:")    
+else 
+    git clone "git@github.com:$1"
+fi
+
+var=$(split_string "$1" "/");
+
+if [[ $var == *'.git' ]]
+then 
+    var=$(trimmerSuffic "$var" ".git")
+fi 
+# echo $var
+cd $var;
+};
+
+
+
+
+split_string() {
+    local string="$1"
+    local delimiter="$2"
+
+    # Saving the current value of IFS
+    local oldIFS=$IFS
+
+    # Setting the delimiter
+    IFS="$delimiter"
+
+    # Splitting the string and reading into variables
+    read -r discard secondPart <<< "$string"
+
+    # Restoring the previous value of IFS
+    IFS=$oldIFS
+
+    # Returning the second part
+    echo "$secondPart"
+}
+
+
+trimmerPrefix() {
+    local string="$1"
+    local prefix="$2"
+    echo "${string#$prefix}";
+}
+
+
+trimmerSuffic(){
+    local string="$1"
+    local suffix="$2"
+    echo "${string%$suffix}";
+
+}
+
+
--- git_functions.sh
+++ git_functions.sh
@@ -1,1 +1,120 @@
-/home/figaro/Programms/Bash/GitShortcuts/git_functions_public.sh
+#!/bin/bash
+
+gall (){
+    
+    if [ $# -eq 0 ]
+    then
+        echo "No arguments supplied"
+        exit 1
+    fi
+    
+    message=""
+    
+    for arg in "$@"; do
+        message+="$arg "
+    done
+    
+    git add . && git commit -m "$message" && git push ;
+    
+}
+
+
+
+gadd () {
+    git add . ;
+}
+
+
+
+gadl () {
+    git add -A
+}
+
+
+
+gco () {
+    
+    if [ $# -eq 0 ]
+    then
+        echo "No arguments supplied"
+        exit 1
+    fi
+    
+    message=""
+    
+    for arg in "$@"; do
+        message+="$arg "
+    done
+    
+    
+    git commit -m "$message"
+    
+}
+
+
+
+gps () {
+    git push
+}
+
+
+
+
+gst () {
+    git status;
+}
+
+
+gcdu(){
+    if [[ $# -eq 0 ]]
+    then
+        echo "You need to specify the directory";
+    else
+        
+        cd $1 || exit 1
+        
+        
+        if [ -d .git ];
+        then
+            
+            
+            find . -type f -size +100M >> .gitignore;
+            
+            if [[ -n $(git status --porcelain) ]]; then
+                message1="Repository has uncommitted changes. Please commit or stash your changes before updating."
+                notify-send 'Git Update Status' "Initiating Adding Commiting and Pushing \n $1" -t 2000
+                echo "$message1"
+                datetime=$(date +"%Y-%m-%d %H:%M:%S")
+                
+                git add . ; git commit -m " back up -  $datetime " ;
+                message_git="$(git push)"
+                if $message_git
+                then
+                    notify-send 'Git Update Status' "Push was successful" -t 2500
+                else
+                    notify-send 'Git Update Status' "Push was NOT successful \n $message_git"
+                fi
+                exit 0
+            fi
+            
+            # Fetch the latest changes from the remote repository
+            git fetch
+            
+            # Check if the current branch is behind the remote tracking branch
+            if [[ -n $(git status -uno | grep 'Your branch is behind') ]]; then
+                message2="Repository is not up to date."
+                notify-send 'Git Update Status' "$message2" -t 2000
+                echo "$message2"
+                exit 0
+            fi
+            
+            message3='Repository is uptodate '
+            notify-send 'Git Update Status' "$message3" -t 2000
+            
+            echo "$message3"
+        else
+            echo "No Git repository Found"
+            notify-send 'Error' 'No Git Repository Found' -t 2000
+        fi
+    fi
+}
